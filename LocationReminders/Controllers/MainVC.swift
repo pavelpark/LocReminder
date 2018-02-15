229116b1783922a299369f3de9a42395b48f5f20
robert-hatfield <robert-hatfield@users.noreply.github.com>
2018-02-14 22:25:46 -0800

    Move files into MVC subfolders, begin adding Swift classes
    Work started on #2 - Switch to Swift from Objective-C
    
    I moved the files into folders to match the Xcode groups.

---
+++ LocationReminders/Controllers/MainVC.swift
@@ -0,0 +1,48 @@
+//
+//  MainVC.swift
+//  LocationReminders
+//
+//  Created by Rob Hatfield on 2/14/18.
+//  Copyright © 2018 Pavel Parkhomey. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MainVC: UIViewController {
+
+    // MARK: Outlets & properties
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var currentLocationButton: UIButton!
+//    var locationController: LocationController
+    var mapShouldFollowUser: Bool = false
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        
+        guard let annotationView = sender as? MKAnnotationView, segue.identifier == "AddReminderViewController" else {
+            return
+        }
+        let newReminderVC = segue.destination
+        
+    }
+
+
+}
---
+++ LocationReminders/Models/Reminder.swift
@@ -0,0 +1,15 @@
+//
+//  Reminder.swift
+//  LocationReminders
+//
+//  Created by Rob Hatfield on 2/14/18.
+//  Copyright © 2018 Pavel Parkhomey. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+struct Reminder {
+    var name: String
+    var region: CLCircularRegion
+}
